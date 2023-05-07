dd61b65defe6efce0c3f51660ea67559f6ad7142
KevinBenson7805 <kevinbenon7805@gmail.com>
2023-05-07 01:23:59 -0700

    map was scaled

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Phaser from 'phaser';
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import map from "./assets/Images/background.png";
 import points from "./assets/Images/points.png";
 import './App.css';
 import mapData from './assets/mapInfo';
 import PointObject from './components/PointsGroup';
 
-document.addEventListener("keydown", (event) => {
-  if (event.ctrlKey && (event.key === "=" || event.key === "-")) {
-    event.preventDefault();
-  }
-});
+
 var startPoint = [-1, -1, -1]
 var endPoint = [-1, -1]
 function App() {
   let game = null;
-  var pointList=[]
+  var pointList = []
+  var ref = useRef(null);
+  const [width, setWidth] = useState(window.outerWidth * 60 / 100)
+  const [height, setHeight] = useState(window.innerHeight);
   function findShortestPath(startNodeIndex, endNodeIndex) {
     const visitedNodes = [];
     const nodesToVisit = [[startNodeIndex, []]];
@@ -44,34 +43,38 @@ function App() {
       return;
     }
     if (startPoint[0] !== -1) {
-      let nodeList=findShortestPath(startPoint[2],index)
+      let nodeList = findShortestPath(startPoint[2], index)
       console.log(nodeList)
-      for(let i=0;i<pointList.length;i++){
-         pointList[i].point.setFrame(0)
-        
+      console.log(pointList.length)
+      for (let i = 0; i < pointList.length; i++) {
+        // console.log(i)
+        pointList[i].point.setFrame(0)
+
       }
-      pointList[nodeList[0]-1].point.setFrame(1)
-      for(let i=1;i<nodeList.length-1;i++){
-        
-        if(nodeList[i]<56){
-          pointList[nodeList[i]-1].point.setFrame(3)
+      pointList[nodeList[0] - 1].point.setFrame(1)
+      for (let i = 1; i < nodeList.length - 1; i++) {
+
+        if (nodeList[i] < 56) {
+          pointList[nodeList[i] - 1].point.setFrame(3)
         }
       }
-      
+
       point.setFrame(1)
       return;
     }
 
   }
-  var clearPath = () =>{
-    startPoint=[-1,-1,-1]
-    endPoint=[-1,-1,-1]
-    for(let i=0;i<pointList.length;i++){
+  var clearPath = () => {
+    startPoint = [-1, -1, -1]
+    endPoint = [-1, -1, -1]
+    for (let i = 0; i < pointList.length; i++) {
       pointList[i].point.setFrame(0)
-     
-   }
+
+    }
   }
   useEffect(() => {
+
+    console.log(width)
     if (game === null) {
       game = new Phaser.Game({
         title: "Game Title",
@@ -90,28 +93,99 @@ function App() {
 
           create: function () {
             const bg = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, "background");
-            console.log(this.cameras.main.centerX, this.cameras.main.centerY)
-            bg.setDisplaySize(1000, 1000);
+            bg.setDisplaySize(width, height);
             let mapPoint = mapData.point;
             for (let i = 1; i < mapPoint.length; i++) {
-              const pointObject = new PointObject(i, mapPoint[i], this, buttonClickHandle);
+              if (mapPoint[i][1][0] < 0) {
+                continue
+              }
+              const pointObject = new PointObject(i, mapPoint[i], this, buttonClickHandle, { width, height });
               pointObject.create()
               pointList.push(pointObject)
             }
+
+            this.input.on('wheel', function (pointer, gameObjects, deltaX, deltaY, deltaZ) {
+              const center = new Phaser.Math.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+              const speed = 0.3;
+              const maxZoomIn = 1;
+              const maxZoomOut = 4;
+
+
+              if (deltaY > 0 && this.cameras.main.zoom > maxZoomIn) {
+                // if (  this.cameras.main.scrollX= -width * (this.cameras.main.zoom - 1) / 2 / this.cameras.main.zoom)
+                
+                 this.cameras.main.zoom -= speed;
+              } else if (deltaY < 0 && this.cameras.main.zoom < maxZoomOut) {
+                this.cameras.main.zoom += speed;
+              }
+              const worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+              const diff = worldPoint.clone().subtract(center);
+              // this.cameras.main.scrollX -= diff.x * 0.0025;
+              // this.cameras.main.scrollY -= diff.y * 0.0025;
+              pointList.map(point => {
+                point.point.setDisplaySize(Math.floor(25 / this.cameras.main.zoom), Math.floor(25 / this.cameras.main.zoom))
+              })
+            }, this);
+            let pointerDown = false;
+            let dragStartX = 0;
+            let dragStartY = 0;
+            let previewX = 0;
+            let previewY = 0;
+            this.input.on('pointerdown', function (pointer) {
+              if (pointer.leftButtonDown() && this.cameras.main.zoom > 1) {
+                pointerDown = true;
+                dragStartX = pointer.x;
+                dragStartY = pointer.y;
+                previewX = pointer.x;
+                previewY = pointer.y;
+                this.input.setDefaultCursor('grab');
+              }
+            }, this);
+
+            this.input.on('pointerup', function (pointer) {
+              if (pointer.leftButtonReleased()) {
+                pointerDown = false;
+                this.input.setDefaultCursor('default');
+              }
+            }, this);
+
+            this.input.on('pointermove', function (pointer) {
+              if (pointerDown) {
+                const diffX = pointer.x - previewX;
+                const diffY = pointer.y - previewY;
+                if (
+                  this.cameras.main.scrollX - diffX / this.cameras.main.zoom >= -width * (this.cameras.main.zoom - 1) / 2 / this.cameras.main.zoom &&
+                  this.cameras.main.scrollX - diffX / this.cameras.main.zoom <= width * (this.cameras.main.zoom - 1) / 2 / this.cameras.main.zoom &&
+                  this.cameras.main.scrollY - diffY / this.cameras.main.zoom >= -height * (this.cameras.main.zoom - 1) / 2 / this.cameras.main.zoom &&
+                  this.cameras.main.scrollY - diffY / this.cameras.main.zoom <= height * (this.cameras.main.zoom - 1) / 2 / this.cameras.main.zoom
+                ) {
+
+                  this.cameras.main.scrollX -= diffX / this.cameras.main.zoom;
+                  this.cameras.main.scrollY -= diffY / this.cameras.main.zoom;
+
+                  previewX = pointer.x
+                  previewY = pointer.y
+                }
+                else
+                  pointerDown=false
+              }
+            }, this);
           }
         }
       });
     }
   }
     , []);
-
   return (
-    <div className='relative w-full'>
+    <div className='relative w-full h-fit' ref={ref}>
 
-      <div id="game-container" className='absolute'>
+      <div className='absolute w-1/5 h-full left-0 m-0'>
+        <button className='w-full h-full bg-blue-500 hover:bg-black' onClick={clearPath}/>
       </div>
-      <div className='w-[1000px] mx-auto'>
-        <button className='absolute w-5 h-5 bg-blue-500 rounded-md hover:bg-black' onClick={clearPath}></button>
+      <div className='absolute w-1/5 h-full right-0 m-0'>
+        <button className='w-full h-full bg-blue-500 hover:bg-black' />
+      </div>
+      <div id="game-container" className=''>
       </div>
       {/* <PointsGroup mapData={mapData} /> */}
     </div>

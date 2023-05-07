52d8252c24d4d410218b520eaea6289667b16c4f
johncoffeocean <coffeocean918@outlook.com>
2023-05-08 04:20:52 +0900

    add new features on gameinfo/monsters :)

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import Header from "./components/global/Header";
 import Footer from "./components/global/Footer";
 import Page404 from "./components/global/Page404";
 import Dashboard from "./components/global/Dashboard";
+import News from "./components/news/News";
+import GameInfo from "./components/gameinfo/GameInfo";
+import Items from "./components/gameinfo/Items";
+import Monsters from "./components/gameinfo/Monsters";
+
 // import Error from "./Components/Error";
 
 function App() {
   return (
     <div className="App relative">
-     
-
       <BrowserRouter>
-      <Header />
+        <Header />
         <Routes>
-          
           <Route path="/" element={<Dashboard />} />
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/new" element={<News />} />
+          <Route path="/gameinfo" element={<GameInfo />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/monsters" element={<Monsters />} />
+          <Route path="/*" element={<Page404 />} />
         </Routes>
-      <Footer />
-
+        <Footer />
       </BrowserRouter>
     </div>
   );
